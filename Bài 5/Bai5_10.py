20cdfb041388980edc2b6f1900fc36f651c18ed5
huy301050 <hoangnghiahuy2005@gmail.com>
2025-04-25 08:53:15 +0700

    Add files via upload

---
+++ Bài 5/Bai5_10.py
@@ -0,0 +1,13 @@
+print("Hoàng Nghĩa Huy")
+print("MSSV:235752020710001")
+
+from sort_module import bubbleSort
+n = int(input("Nhập số lượng phần tử trong danh sách: "))
+nlist = []
+for i in range(n):
+  value = float(input(f"Nhập phần tử thứ {i+1}: "))
+  nlist.append(value)
+
+sorted_list = bubbleSort(nlist)
+
+print("Danh sách sau khi sắp xếp:", sorted_list)
---
+++ Bài 5/Bai5_4.py
@@ -0,0 +1,9 @@
+print("Hoàng Nghĩa Huy")
+print("MSSV:235752020710001")
+
+import numpy as np
+mang = np.arange(12, 39)
+print("Mảng ban đầu:",mang)
+
+mang_dao_nguoc = np.flip(mang)
+print("Mảng đảo ngược:", mang_dao_nguoc)
